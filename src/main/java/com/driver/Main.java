0783731a5b906a4713a9cade00c0fdaf90f8b6ec
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-07-13 19:09:54 +0530

    Solved Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
-public class Main {
+class Product{
+    public int product(int x, int y) {
+        return x + y;
+    }
+    public int product(int x, int y, int z) {
+        return x + y + z;
+    }
+    public double product(double x, double y) {
+        return x + y;
+    }
+}
 
+public class Main {
+    
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans1 = p.product(2, 3);
+        int ans2 = p.product(2, 3, 4);
+        double ans3 = p.product(2.5, 2.6);
+        System.out.println(ans1);
+        System.out.println(ans2);
+        System.out.println(ans3);
+    }
 }
