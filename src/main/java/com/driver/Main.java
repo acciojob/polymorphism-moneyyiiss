5ac3b9c09c7be49bf7f87fc9237481d9f2c2bba2
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-07-14 08:02:25 +0530

    updated code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,32 +1,33 @@
 package com.driver;
 
 //Task1
-class Product{
-    public int product(int x, int y) {
-        return x * y;
-    }
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-    public double product(double x, double y) {
-        return x * y;
-    }
-}
+
 
 public class Main {
+
+    public static class  Product{
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
     
     public static void main(String[] args) {
-//Task2
+        //Task2
         Product p = new Product();
         //Task3
-        int result1  = p.product(2, 3);
-        System.out.println(result1);
+        p.product(2, 3);
         //Task4
-        int result2  = p.product(2, 3, 4);
-        System.out.println(result2);
+        p.product(2, 3, 4);
+        
         //Task5
-        double result3  = p.product(2.5, 2.6);
-        System.out.println(result3);
+        p.product(2.5, 2.6);
+    
         
         
     }
